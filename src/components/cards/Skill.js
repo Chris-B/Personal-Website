fe412bc43720dc665d82b6ef622796135dd3ea50
Chris <crbarcla@uci.edu>
2019-08-20 19:39:14 -0700

    Added href's for skill cards.

--- src/components/cards/Skill.js
+++ src/components/cards/Skill.js
@@ -5,7 +5,9 @@ class Skill extends Component {
     render() {
         return (
             <div className="skill-wrapper">
-                <img class={"skill-img"} src={"/resources/skills/".concat(this.props.thumbnail)} alt={this.props.imgalt} />
+                <a target={"_blank"} href={this.props.imglink}>
+                    <img class={"skill-img"} src={"/resources/skills/".concat(this.props.thumbnail)} alt={this.props.imgalt} />
+                </a>
                 <h3 class={"skill-title"}>{this.props.title}</h3>
             </div>
         );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,19 +12,19 @@ class Home extends Component {
                         <h1>Software Engineer</h1>
                         <h2>Desktop and Web Application Developer</h2>
                         <div className="skill-cards">
-                            <Skill thumbnail={"java.png"} imgalt={"java"} title={"Java"}/>
-                            <Skill thumbnail={"python.png"} imgalt={"python"} title={"Python"}/>
-                            <Skill thumbnail={"c++.png"} imgalt={"c++"} title={"C++"}/>
-                            <Skill thumbnail={"csharp.png"} imgalt={"c#"} title={"C#"}/>
-                            <Skill thumbnail={"javascript.png"} imgalt={"javascript"} title={"JavaScript"}/>
-                            <Skill thumbnail={"html.png"} imgalt={"html"} title={"HTML"}/>
-                            <Skill thumbnail={"css.png"} imgalt={"css"} title={"CSS"}/>
-                            <Skill thumbnail={"php.png"} imgalt={"php"} title={"PHP"}/>
-                            <Skill thumbnail={"node.png"} imgalt={"node"} title={"Node.js"}/>
-                            <Skill thumbnail={"react.png"} imgalt={"react"} title={"React.js"}/>
-                            <Skill thumbnail={"sql.png"} imgalt={"sql"} title={"SQL"}/>
-                            <Skill thumbnail={"git.png"} imgalt={"git"} title={"Git"}/>
-                            <Skill thumbnail={"linux.png"} imgalt={"linux"} title={"Linux"}/>
+                            <Skill thumbnail={"java.png"} imgalt={"java"} title={"Java"} imglink={"https://www.java.com/"}/>
+                            <Skill thumbnail={"python.png"} imgalt={"python"} title={"Python"} imglink={"https://www.python.org/"}/>
+                            <Skill thumbnail={"c++.png"} imgalt={"c++"} title={"C++"} imglink={"http://www.cplusplus.com/"}/>
+                            <Skill thumbnail={"csharp.png"} imgalt={"c#"} title={"C#"} imglink={"https://dotnet.microsoft.com/"}/>
+                            <Skill thumbnail={"javascript.png"} imgalt={"javascript"} title={"JavaScript"} imglink={"https://www.javascript.com/"}/>
+                            <Skill thumbnail={"html.png"} imgalt={"html"} title={"HTML"} imglink={"https://en.wikipedia.org/wiki/HTML"}/>
+                            <Skill thumbnail={"css.png"} imgalt={"css"} title={"CSS"} imglink={"https://en.wikipedia.org/wiki/Cascading_Style_Sheets"}/>
+                            <Skill thumbnail={"php.png"} imgalt={"php"} title={"PHP"} imglink={"https://www.php.net/"}/>
+                            <Skill thumbnail={"node.png"} imgalt={"node"} title={"Node.js"} imglink={"https://nodejs.org/"}/>
+                            <Skill thumbnail={"react.png"} imgalt={"react"} title={"React.js"} imglink={"https://reactjs.org/"}/>
+                            <Skill thumbnail={"sql.png"} imgalt={"sql"} title={"SQL"} imglink={"https://en.wikipedia.org/wiki/SQL"}/>
+                            <Skill thumbnail={"git.png"} imgalt={"git"} title={"Git"} imglink={"https://git-scm.com/"}/>
+                            <Skill thumbnail={"linux.png"} imgalt={"linux"} title={"Linux"} imglink={"https://en.wikipedia.org/wiki/Linux"}/>
                         </div>
                     </div>
                 </div>
