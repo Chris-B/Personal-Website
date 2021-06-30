0ba67d937a8c2c38d34b875fdeaa64ab1cc8eda0
chris <chris@chrisbarclay.dev>
2021-06-30 00:23:09 -0600

    Updated AutoRune link

--- src/screens/Portfolio.js
+++ src/screens/Portfolio.js
@@ -7,7 +7,7 @@ class Portfolio extends Component {
             <div className="portfolio-wrapper">
                 <div className="project-cards">
                     <Project thumbnail={"site.png"} imgalt={"website"} title={"This Site"} projlink={"https://github.com/Chris-B/Personal-Website"}/>
-                    <Project thumbnail={"autorune.png"} imgalt={"autorune"} title={"AutoRune"} projlink={"https://github.com/Chris-AutoRune"}/>
+                    <Project thumbnail={"autorune.png"} imgalt={"autorune"} title={"AutoRune"} projlink={"https://github.com/AutoRune-OSRS"}/>
                     <Project thumbnail={"farmer.jpg"} imgalt={"famerjohn"} title={"Farmer John Website"} projlink={"http://www.farmerjohn.cc/"}/>
                     <Project thumbnail={"vm.png"} imgalt={"vmsys"} title={"Virtual Memory System"} projlink={"https://github.com/Chris-B/Virtual-Memory-System"}/>
                     <Project thumbnail={"process.png"} imgalt={"prmgr"} title={"Process/Resource Manager"} projlink={"https://github.com/Chris-B/Process-and-Resource-Manager"}/>
